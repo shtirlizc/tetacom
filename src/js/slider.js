919170d0540e0a8d60f7edd83e12c635a5f0911d
Marat Sarvarov <shtirlizc@gmail.com>
2021-11-12 00:24:58 +0500

    start screen - desktop

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,9 +1,43 @@
-import Swiper from "swiper";
+import Swiper, { Autoplay, EffectFade } from "swiper";
 
 export const Slider = {
+  buttons: document.querySelectorAll("._block-head__pagination-item button"),
+
   init() {
-    new Swiper(".js-swiper-main", {
-      loop: true,
+    this.slider = new Swiper(".js-swiper-main", {
+      modules: [Autoplay, EffectFade],
+      speed: 1000,
+      autoplay: {
+        delay: 7000,
+      },
+      effect: "fade",
+      fadeEffect: {
+        crossFade: true,
+      },
+    });
+
+    this.buttons.forEach((btn) => {
+      btn.addEventListener("click", this.pagination.bind(this));
+    });
+
+    this.slider.on("slideChangeTransitionStart", () => {
+      this.paginationClasses(this.slider.activeIndex);
+    });
+  },
+
+  pagination(evt) {
+    const targetSlideNumber = Number(evt.currentTarget.dataset.slide);
+    this.slider.slideTo(targetSlideNumber);
+    this.paginationClasses(targetSlideNumber);
+  },
+
+  paginationClasses(idx) {
+    this.buttons.forEach((btn) => {
+      if (Number(btn.dataset.slide) === idx) {
+        btn.classList.add("active");
+      } else {
+        btn.classList.remove("active");
+      }
     });
   },
 };
