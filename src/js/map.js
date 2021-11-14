182f10f18e7580bd4d235977ce283059efba231f
Marat Sarvarov <shtirlizc@gmail.com>
2021-11-14 17:29:01 +0500

    update map

--- src/js/map.js
+++ src/js/map.js
@@ -4,25 +4,43 @@ export function map() {
   }
 
   function init() {
-    var myMap = new ymaps.Map("map", {
-        center: [59.89292230823245, 30.317175747313602],
-        zoom: 17,
-      }),
-      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>',
-      );
+    ymaps.ready(function () {
+      var myMap = new ymaps.Map(
+          "map",
+          {
+            center: [54.736984, 55.946816],
+            zoom: 17,
+          },
+          {
+            searchControlProvider: "yandex#search",
+          },
+        ),
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>',
+        ),
+        myPlacemark = new ymaps.Placemark(
+          myMap.getCenter(),
+          {
+            hintContent: "Тетаком: облачные сервисы для нефтегазовой отрасли",
+          },
+          {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: "default#image",
+            // Своё изображение иконки метки.
+            iconImageHref: "",
+            // Размеры метки.
+            iconImageSize: [50, 60],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-25, -60],
+          },
+        );
 
-    myMap.geoObjects.add(
-      new ymaps.Placemark(
-        [59.89292230823245, 30.317175747313602],
-        {
-          balloonContent: "Установка, обслуживание и реставрация бассейнов любого типа и размеров",
-        },
-        {
-          preset: "islands#circleIcon",
-          iconColor: "#4699f8",
-        },
-      ),
-    );
+      myMap.behaviors.disable("scrollZoom");
+      myMap.geoObjects.add(myPlacemark);
+      myMap.options.set("suppressMapOpenBlock", true);
+    });
   }
 }
