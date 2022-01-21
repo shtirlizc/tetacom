1c791a8d7a21548ff41cce519e9294a7ec0f0b63
Marat Sarvarov <shtirlizc@gmail.com>
2022-01-21 22:27:42 +0500

    add sponsor link, show languages dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ import { Slider } from "./slider";
 import { PageYOffset } from "./pageYOffset";
 import { Modal } from "./modal";
 import { Form } from "./modal";
-// import { Dropdown } from "./dropdown";
+import { Dropdown } from "./dropdown";
 
 window.addEventListener("load", () => {
   mobileMenu();
@@ -21,5 +21,5 @@ window.addEventListener("load", () => {
   PageYOffset.init();
   Modal.init();
   Form.init();
-  // Dropdown.init();
+  Dropdown.init();
 });
