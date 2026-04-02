ed9f3fa9666a38426336f25a022dfd2e302907b5
Marat Sarvarov <shtirlizc@gmail.com>
2026-04-02 17:02:34 +0500

    robots, sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tetacom.org',
+  integrations: [sitemap()],
+  redirects: {
+    "/404": "/",
+  },
   i18n: {
     locales: ['ru', 'en'],
     defaultLocale: 'ru',
